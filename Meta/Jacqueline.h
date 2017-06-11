f9591367119f6d3f5dec71cbbaf33e238641bffb
Oscar Fuentes <oscar@appwinit.com>
2017-06-10 21:01:22 -0400

    Cleanup.

---
+++ Meta/Jacqueline.h
@@ -0,0 +1,19 @@
+//
+//  Jacqueline.h
+//  Jacqueline
+//
+//  Created by Oscar Fuentes on 6/10/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Jacqueline.
+FOUNDATION_EXPORT double JacquelineVersionNumber;
+
+//! Project version string for Jacqueline.
+FOUNDATION_EXPORT const unsigned char JacquelineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jacqueline/PublicHeader.h>
+
+
